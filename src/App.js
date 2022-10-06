e4db14318fa24868ce4ea4f3efef560129e27207
minju0809 <loverminju88@naver.com>
2022-10-06 19:08:34 +0900

    7주차 ~2/5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+import ClassComponent from './components/2-5.State/ClassComponent';
+import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
+import Extraction from './components/Extraction/Extraction';
+import Composition from './components/2-4.Props/Composition';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <FunctionalComponent /> */}
+      {/* <ClassComponent /> */}
+      <ClassComponent2 />
+      {/* <Extraction /> */}
+      {/* <Composition /> */}
     </div>
   );
 }
